930b0028ff90ad46b51eb4421db7f5ed1335285b
AliSinan1 <129323859+AliSinan1@users.noreply.github.com>
2023-09-25 18:44:47 +0300

    Delete One Sort Method

--- Sorter.java
+++ Sorter.java
@@ -36,7 +36,4 @@ public class Sorter {
     public static void sort(int[] array){
         insertionSort(array);
     }
-    public static void sort(int[] array){
-        selectionSort(array);
-    }
 }  
